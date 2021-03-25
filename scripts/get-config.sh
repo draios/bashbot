f1dcb31d98e96674b54ddced686022bb522bc5e4
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2021-03-25 14:09:52 +0100

    Add debugging info

--- scripts/get-config.sh
+++ scripts/get-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2086
-set -eou pipefail
+set -xeou pipefail
 
 expected_variables="GIT_TOKEN github_org github_repo github_branch github_filename"
 for expect in $expected_variables; do
@@ -10,8 +10,8 @@ for expect in $expected_variables; do
   fi
 done
 
-curl -H "Authorization: token $GIT_TOKEN" \
+curl -v -H "Authorization: token $GIT_TOKEN" \
   -H 'Accept: application/vnd.github.v3.raw' \
   -o ../config.json \
-  -sL https://api.github.com/repos/${github_org}/${github_repo}/contents/${github_filename}?ref=${github_branch} 2>&1
+  -sL https://api.github.com/repos/${github_org}/${github_repo}/contents/${github_filename}?ref=${github_branch}
 
